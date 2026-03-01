a3297ec0309c2ce6cff82bf0896ba09d848baf01
josejesusgarciajr <josejesusgarciajr@gmail.com>
2026-03-01 13:05:37 -0800

    Auto play videos on APOD Service on APODDisplay Component Mount and whenever apod updates

--- src/NASA_Components/APODDisplay.tsx
+++ src/NASA_Components/APODDisplay.tsx
@@ -1,5 +1,6 @@
 import { NASAServiceDisplay } from '../NASA_Components/NASAServiceDisplay'
 import type { APOD } from '../types/NASA/APOD'
+import { useRef, useEffect } from 'react'
 
 type APODDisplayProps = {
     apod: APOD;
@@ -9,6 +10,14 @@ export const APODDisplay = ({apod} : APODDisplayProps) => {
     const video = apod.media_type === 'video';
     const secureVideoLink = apod.url?.replace('http://', 'https://');
     const secureImageLink = apod.hdurl?.replace('http://', 'https://');
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    // play video on app mount
+    useEffect(() => {
+        if (video && videoRef.current) {
+            videoRef.current.play();
+        }
+    }, [apod]);
 
     return (
         <>
@@ -16,7 +25,7 @@ export const APODDisplay = ({apod} : APODDisplayProps) => {
             <p style={{ textAlign: 'center' }}>{apod.title}</p>
             <p style={{ textAlign: 'center' }}>{apod.date}</p>
             { video ? (
-                <video src={secureVideoLink} controls width='100%'/>
+                <video ref={videoRef} src={secureVideoLink} controls width='100%' muted />
             ) : (
                 <img src={secureImageLink} alt={apod.title}
                     style={{ width: '100%', maxWidth: '800px', margin: '0 auto', display: 'block' }} 
